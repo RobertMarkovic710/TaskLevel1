3f75b12cc98eceeabc93b4aee0cb246d6ba7d4f0
RobertMarkovic710 <rmarkovic70@gmail.com>
2016-11-19 19:36:51 +0100

    done update

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;         //int only numbers, string can do booth
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program         //ctrl + F5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please ENLIST or DISPLAY: ");
+            String ChooseOperation = (Console.ReadLine());
+
+            if (ChooseOperation == "enlist" )
+            {
+                Console.WriteLine("Please write your first name: ");
+                String StudentFirstName = Console.ReadLine();
+
+                Console.WriteLine("Please write your last name: ");
+                String StudentLastName = Console.ReadLine();
+
+                Console.WriteLine("Please write your GPA: ");
+                String StudentGPA = Console.ReadLine();
+            }
+            else if (ChooseOperation == "display")
+            {
+ 
+            }
+        }
+    }
+}
